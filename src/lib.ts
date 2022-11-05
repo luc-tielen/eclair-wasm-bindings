f8fc2d863b7e24a7ad5df8cb9429367bbc224c81
Luc Tielen <luc.tielen@gmail.com>
2022-11-05 10:13:45 +0100

    Get more things working

--- src/lib.ts
+++ src/lib.ts
@@ -1,47 +1,5 @@
 // POC code
 
-/*
-enum Direction {
-  INPUT,
-  OUTPUT,
-  INPUT_OUTPUT,
-}
-
-enum FieldType {
-  U32,
-  String,
-}
-
-type Fields = Record<string, FieldType>;
-
-type Fact<Name extends string, Dir extends Direction, F extends Fields> = {
-  name: Name;
-  direction: Dir;
-} & F;
-
-type Edge = Fact<
-  "edge",
-  Direction.INPUT,
-  { from: FieldType.U32; to: FieldType.U32 }
->;
-
-type Reachable = Fact<
-  "reachable",
-  Direction.OUTPUT,
-  { from: FieldType.U32; to: FieldType.U32 }
->;
-
-// TODO how to constrain these generics?
-type Program<Name, Facts> = {
-  name: Name;
-  facts: Facts;
-};
-
-type Path = Program<"path", [Edge, Reachable]>;
-
-type X = Path["facts"];
-*/
-
 enum Direction {
   INPUT,
   OUTPUT,
@@ -53,40 +11,52 @@ enum FieldType {
   String = "STRING",
 }
 
-const number = () => FieldType.Number;
-const string = () => FieldType.String;
+const number = (): FieldType.Number => FieldType.Number;
+const string = (): FieldType.String => FieldType.String;
 
-// TODO
-const SERIALIZERS: {
-  [FieldType.Number]: (x: number) => void;
-  [FieldType.String]: (x: string) => void;
-} = {
-  [FieldType.Number]: () => { },
-  [FieldType.String]: () => { },
+// TODO implement
+const SERIALIZERS = {
+  [FieldType.Number]: (value: number) => { },
+  [FieldType.String]: (value: string) => { },
 };
 
-const DESERIALIZERS: {
-  [FieldType.Number]: () => number;
-  [FieldType.String]: () => string;
-} = {
+const DESERIALIZERS = {
   [FieldType.Number]: () => 42,
   [FieldType.String]: () => "abc",
 };
 
-// Order needs to be preserved!
-type Fields = FieldType[];
+// Order needs to be preserved, so we can't use object?
+// Or do we need to explicitly specify location at the type level?
+type FactShape = [FieldType, ...FieldType[]];
 
+// Transforms the enum type into the actual underlying column type
 type ColumnType<T extends FieldType> = T extends FieldType.Number
   ? number
   : string;
 
-type FactValue<T extends FieldType[]> = {
+// Type representing a single fact value
+type FactValue<T extends FactShape> = {
   [Property in keyof T]: ColumnType<T[Property]>;
 };
 
-type X = FactValue<[FieldType.Number, FieldType.String]>;
+type FactMetadata<
+  Name extends string,
+  Dir extends Direction,
+  Shape extends FactShape
+> = {
+  name: Name;
+  dir: Dir;
+  fields: Shape;
+  // TODO rename to serialize / deserialize?
+  addFact: (fact: FactValue<Shape>) => void;
+  getFact: () => FactValue<Shape>;
+};
 
-const fact = <T>(name: string, dir: Direction, fields: Fields) => {
+const fact = <Name extends string, Dir extends Direction, T extends FactShape>(
+  name: Name,
+  dir: Dir,
+  fields: T
+): FactMetadata<Name, Dir, T> => {
   const serializers = fields.map((field) => SERIALIZERS[field]);
   const deserializers = fields.map((field) => DESERIALIZERS[field]);
 
@@ -94,84 +64,93 @@ const fact = <T>(name: string, dir: Direction, fields: Fields) => {
     name,
     dir,
     fields,
-    addFact: (fact: FactValue<T>) => {
+    addFact: (fact) => {
       for (const k in fact) {
         const field = fields[k];
         const value = fact[k];
         serializers[field](value);
       }
     },
-    getFact: (): FactValue<T> =>
+    getFact: () =>
       fields.map((field) => deserializers[field]()) as FactValue<T>,
   };
 };
 
-const edge = fact("edge", Direction.INPUT, [
-  FieldType.Number,
-  FieldType.String,
-]);
-/*
-interface Literal<T> {
-  shape: () => T;
+interface FactHandler<Dir extends Direction, Shape extends FactShape> {
+  dir: Dir;
+  addFact: (fact: FactValue<Shape>) => void;
+  addFacts: (fact: FactValue<Shape>[]) => void;
+  getFacts: () => FactValue<Shape>[];
 }
 
-// TODO how to add other fields
-const f = <T>(fields: T): Literal<T> => {
-  return { shape: () => fields };
+type Program<T> = {
+  [Key in keyof T]: T[Key] extends FactMetadata<
+    string, // TODO no need to keep track of string at the type level?
+    infer Dir extends Direction,
+    infer Shape extends FactShape
+  >
+  ? FactHandler<Dir, Shape>
+  : never;
 };
-*/
-/* TODO try again in a bit
-const fact = <T extends Fields>(name: string, dir: Direction, fields: T) => {
-  const serializers = Object.fromEntries(
-    Object.entries(fields).map(([key, field]) => [key, SERIALIZERS[field]])
+
+// TODO pass code as first arg
+const program = <T extends Object>(facts: T): Program<T> => {
+  const result = Object.fromEntries(
+    Object.entries(facts).map(([factName, factMetadata]) => {
+      type Key = keyof T;
+
+      type FactType = T[Key] extends FactMetadata<
+        string,
+        Direction,
+        infer Shape
+      >
+        ? FactValue<Shape>
+        : never;
+
+      // type Handler = T[Key] extends FactMetadata<string, infer Dir, infer Shape>
+      //   ? FactHandler<Dir, Shape>
+      //   : never;
+      type FactMD = T[Key] extends FactMetadata<string, infer Dir, infer Shape>
+        ? FactMetadata<string, Dir, Shape>
+        : never;
+      const factData = factMetadata as FactMD;
+      return [
+        factName,
+        {
+          ...([Direction.INPUT, Direction.INPUT_OUTPUT].includes(
+            factData.dir
+          ) && {
+            addFact: factMetadata.addFact,
+            addFacts: (facts: FactType[]) =>
+              facts.forEach(factMetadata.addFact),
+          }),
+          ...([Direction.OUTPUT, Direction.INPUT_OUTPUT].includes(
+            factData.dir
+          ) && {
+            getFacts: () => {
+              const count = 10; // TODO use count call
+              const result: FactType[] = [];
+              for (let i = 0; i < count; i++) {
+                result.push(factData.getFact() as FactType);
+              }
+              return result;
+            },
+          }),
+        },
+      ];
+    })
   );
 
-  return {
-    name,
-    dir,
-    fields,
-    addFact: (fact: FactValue<T>) => {
-      Object.entries(fact).forEach(([key, value]) => {
-        const serializer = serializers[key]
-        serializer(value)
-      })
-
-
-      Object.entries(fields).map(([key, fieldType]) => {
-        switch (fieldType) {
-          case FieldType.Number: {
-            const value: boolean = fact[key]; // TODO
-            console.log(value);
-            break;
-          }
-          case FieldType.String: {
-            const value: string = fact[key]; // TODO
-            console.log(value);
-            // TODO
-            break;
-          }
-        }
-      });
-    },
-  };
+  return result as unknown as Program<T>;
 };
-*/
 
-/*
-const program = ({ code, facts }) => { };
-
-// fields
-const edge = fact("edge", Direction.INPUT);
-const reachable = fact("reachable", Direction.OUTPUT);
-const path = program({ code: null, facts: [edge, reachable] });
-
-path.edge.addFact([
-  { from: 1, to: 2 },
-  { from: 2, to: 3 },
+const edge = fact("edge", Direction.INPUT, [
+  FieldType.Number,
+  FieldType.Number,
 ]);
-path.edge.addFacts([{ from: 2, to: 3 }]);
-
-// reachable should be type error
-const reachables = path.reachable.getFacts();
-
-reachables.forEach(console.log);*/
+const reachable = fact("reachable", Direction.OUTPUT, [
+  FieldType.Number,
+  FieldType.Number,
+]);
+const path = program({ edge, reachable });
+console.log(path.edge);
